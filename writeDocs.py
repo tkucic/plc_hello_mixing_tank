57da73d9e3be89cd25cfee9683d27f25f2579032
Toni Kucic <tkucic@gmail.com>
2021-03-09 20:46:47 +0200

    Update iecMd docs

--- writeDocs.py
+++ writeDocs.py
@@ -4,6 +4,6 @@ proj = ia_tools.Project('plc_hello_mixing_tank.xml')
 
 proj.Data['info']['companyName'] = 'TK Automation'
 proj.Data['info']['companyURL'] = 'https://github.com/tkucic'
-proj.Data['info']['contentDescription'] = 'This project is implementing a simple mixing tank with simulator. The purpose of the project is to serve as an educational platform for begginers. The project can be used under the MIT license'
+proj.Data['info']['contentDescription'] = 'In idustrial automation the "hello world" program is a mixing tank. As a part of my portfolio I have written a simple mixing tank controller and a simulator. The project is meant for begginers and for educational purposes. It is published under MIT license. Written in CODESYS 3.5 and in structured text language.\nThe project is designed in a way that the user can disable the main program and replace it with his own implementation. Simulator and the HMI sees only the IO and HMI global variables so if the user wishes to create an own implementation those variables should be written. On the simulator side the operator can see if the implementation is working correctly.'
 
 proj.export('docs', 'iecMd')
